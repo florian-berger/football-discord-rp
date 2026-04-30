e2bc756248b387fc64f33235e6085cf7943cd01e
Florian Berger <dev@florian-berger.info>
2026-05-01 00:54:40 +0200

    [Add] World Cup 2026 and German third division

--- football-discord-rp/DataStore.cs
+++ football-discord-rp/DataStore.cs
@@ -212,12 +212,378 @@
         };
 
         /// <summary>
+        ///     Representing the third German division
+        /// </summary>
+        private static readonly League LeagueGer3 = new()
+        {
+            Name = "3. Liga",
+            Season = "2025/26",
+            Clubs = new List<Club>
+            {
+                new()
+                {
+                    FullName = "VfL Osnabrück",
+                    ShortName = "OSN"
+                },
+                new()
+                {
+                    FullName = "Energie Cottbus",
+                    ShortName = "ENE"
+                },
+                new()
+                {
+                    FullName = "Rot-Weiss Essen",
+                    ShortName = "RWE"
+                },
+                new()
+                {
+                    FullName = "MSV Duisburg",
+                    ShortName = "DUI"
+                },
+                new()
+                {
+                    FullName = "Hansa Rostock",
+                    ShortName = "FCH"
+                },
+                new()
+                {
+                    FullName = "SC Verl",
+                    ShortName = "SCV"
+                },
+                new()
+                {
+                    FullName = "Alemannia Aachen",
+                    ShortName = "ALE"
+                },
+                new()
+                {
+                    FullName = "1860 München",
+                    ShortName = "M60"
+                },
+                new()
+                {
+                    FullName = "Wehen Wiesbaden",
+                    ShortName = "WIE"
+                },
+                new()
+                {
+                    FullName = "Waldhof Mannheim",
+                    ShortName = "SVW"
+                },
+                new()
+                {
+                    FullName = "Viktoria Köln",
+                    ShortName = "VIK"
+                },
+                new()
+                {
+                    FullName = "Jahn Regensburg",
+                    ShortName = "REG"
+                },
+                new()
+                {
+                    FullName = "FC Ingolstadt",
+                    ShortName = "FCI"
+                },
+                new()
+                {
+                    FullName = "VfB Stuttgart II",
+                    ShortName = "VFB"
+                },
+                new()
+                {
+                    FullName = "1. FC Saarbrücken",
+                    ShortName = "FCS"
+                },
+                new()
+                {
+                    FullName = "1899 Hoffenheim II",
+                    ShortName = "HOF"
+                },
+                new()
+                {
+                    FullName = "TSV Havelse",
+                    ShortName = "HAV"
+                },
+                new()
+                {
+                    FullName = "SSV Ulm",
+                    ShortName = "ULM"
+                },
+                new()
+                {
+                    FullName = "Erzgebirge Aue",
+                    ShortName = "AUE"
+                },
+                new()
+                {
+                    FullName = "1. FC Schweinfurt",
+                    ShortName = "S05"
+                }
+            }
+        };
+
+        /// <summary>
+        ///     Representing the FIFA World Cup
+        /// </summary>
+        private static readonly League FifaWorldCup = new()
+        {
+            Name = "FIFA World Cup",
+            Season = "2026",
+            Clubs = new List<Club>
+            {
+                new()
+                {
+                    FullName = "Canada",
+                    ShortName = "CAN"
+                },
+                new()
+                {
+                    FullName = "Mexico",
+                    ShortName = "MEX"
+                },
+                new()
+                {
+                    FullName = "USA",
+                    ShortName = "USA"
+                },
+                new()
+                {
+                    FullName = "Algeria",
+                    ShortName = "DZA"
+                },
+                new()
+                {
+                    FullName = "Argentina",
+                    ShortName = "ARG"
+                },
+                new()
+                {
+                    FullName = "Australia",
+                    ShortName = "AUS"
+                },
+                new()
+                {
+                    FullName = "Austria",
+                    ShortName = "AUT"
+                },
+                new()
+                {
+                    FullName = "Belgium",
+                    ShortName = "BEL"
+                },
+                new()
+                {
+                    FullName = "Bosnia-Herzegovina",
+                    ShortName = "BIH"
+                },
+                new()
+                {
+                    FullName = "Brazil",
+                    ShortName = "BRA"
+                },
+                new()
+                {
+                    FullName = "Cabo Verde",
+                    ShortName = "CPV"
+                },
+                new()
+                {
+                    FullName = "Colombia",
+                    ShortName = "COL"
+                },
+                new()
+                {
+                    FullName = "Congo DR",
+                    ShortName = "COD"
+                },
+                new()
+                {
+                    FullName = "Côte d'Ivoire",
+                    ShortName = "CIV"
+                },
+                new()
+                {
+                    FullName = "Croatia",
+                    ShortName = "HRV"
+                },
+                new()
+                {
+                    FullName = "Curaçao",
+                    ShortName = "CUW"
+                },
+                new()
+                {
+                    FullName = "Czechia",
+                    ShortName = "CZE"
+                },
+                new()
+                {
+                    FullName = "Ecuador",
+                    ShortName = "ECU"
+                },
+                new()
+                {
+                    FullName = "Egypt",
+                    ShortName = "EGY"
+                },
+                new()
+                {
+                    FullName = "England",
+                    ShortName = "ENG"
+                },
+                new()
+                {
+                    FullName = "France",
+                    ShortName = "FRA"
+                },
+                new()
+                {
+                    FullName = "Germany",
+                    ShortName = "DEU"
+                },
+                new()
+                {
+                    FullName = "Ghana",
+                    ShortName = "GHA"
+                },
+                new()
+                {
+                    FullName = "Haiti",
+                    ShortName = "HTI"
+                },
+                new()
+                {
+                    FullName = "IR Iran",
+                    ShortName = "IRN"
+                },
+                new()
+                {
+                    FullName = "Iraq",
+                    ShortName = "IRQ"
+                },
+                new()
+                {
+                    FullName = "Japan",
+                    ShortName = "JPN"
+                },
+                new()
+                {
+                    FullName = "Jordan",
+                    ShortName = "JOR"
+                },
+                new()
+                {
+                    FullName = "Korea Republic",
+                    ShortName = "KOR"
+                },
+                new()
+                {
+                    FullName = "Morocco",
+                    ShortName = "MAR"
+                },
+                new()
+                {
+                    FullName = "Netherlands",
+                    ShortName = "NLD"
+                },
+                new()
+                {
+                    FullName = "New Zealand",
+                    ShortName = "NZL"
+                },
+                new()
+                {
+                    FullName = "Norway",
+                    ShortName = "NOR"
+                },
+                new()
+                {
+                    FullName = "Panama",
+                    ShortName = "PAN"
+                },
+                new()
+                {
+                    FullName = "Paraguay",
+                    ShortName = "PRY"
+                },
+                new()
+                {
+                    FullName = "Portugal",
+                    ShortName = "PRT"
+                },
+                new()
+                {
+                    FullName = "Qatar",
+                    ShortName = "QAT"
+                },
+                new()
+                {
+                    FullName = "Saudi Arabia",
+                    ShortName = "SAU"
+                },
+                new()
+                {
+                    FullName = "Scotland",
+                    ShortName = "SCO"
+                },
+                new()
+                {
+                    FullName = "Senegal",
+                    ShortName = "SEN"
+                },
+                new()
+                {
+                    FullName = "South Africa",
+                    ShortName = "ZAF"
+                },
+                new()
+                {
+                    FullName = "Spain",
+                    ShortName = "ESP"
+                },
+                new()
+                {
+                    FullName = "Sweden",
+                    ShortName = "SWE"
+                },
+                new()
+                {
+                    FullName = "Switzerland",
+                    ShortName = "CHE"
+                },
+                new()
+                {
+                    FullName = "Tunisia",
+                    ShortName = "TUN"
+                },
+                new()
+                {
+                    FullName = "Türkiye",
+                    ShortName = "TUR"
+                },
+                new()
+                {
+                    FullName = "Uruguay",
+                    ShortName = "URY"
+                },
+                new()
+                {
+                    FullName = "Uzbekistan",
+                    ShortName = "UZB"
+                }
+            }
+        };
+        
+        /// <summary>
         ///     List of all available leagues
         /// </summary>
         public static List<League> Leagues { get; set; } =
         [
             LeagueGer1,
-            LeagueGer2
+            LeagueGer2,
+            LeagueGer3,
+            FifaWorldCup
         ];
     }
 }
